4f1b74d858077837d53ec407079815da62840d2f
VitorBressan <contato.vbressan@gmail.com>
2026-01-19 15:59:27 -0300

    refactor: improve win detection

--- game_engine.py
+++ game_engine.py
@@ -7,11 +7,16 @@ class GameEngine():
         self.playerA_name: str = ""
         self.playerB_name: str = ""
         self.current_player: str = ""
+        self.moves_done: dict = {}
     
     def set_names(self, player1_name, player2_name):
         self.playerA_name = player1_name
         self.playerB_name = player2_name
         self.current_player = self.playerA_name
+        self.moves_done = {
+            self.playerA_name: [],
+            self.playerB_name: []
+        }
 
     def is_valid_move(self, move):
         if move < 1 or move > 9:
@@ -35,39 +40,31 @@ class GameEngine():
     def make_move(self, move):
         if self.is_valid_move(move):
             self.board[move - 1] = self.mark_slot()
+            self.moves_done[self.current_player].append(move)
             return True
         else:
             return False
     
     def check_for_win(self):
-        #[0][1][2]
-        #[3][4][5]
-        #[6][7][8]
-
-        # Horizontal Win
-        if self.board[0] == self.board[1] == self.board[2] and self.board[0] != "":
-            return True
-        elif self.board[3] == self.board[4] == self.board[5] and self.board[3] != "":
-            return True
-        elif self.board[6] == self.board[7] == self.board[8] and self.board[6] != "":
-            return True
+        win_combinations = [
+            # lines
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            #columns
+            {1, 4, 7},
+            {2, 5, 8},
+            {3, 6, 9},
+            #diagonals
+            {1, 5, 9},
+            {3, 5, 7}
+        ]
         
-        # Vertical Win
-        if self.board[0] == self.board[3] == self.board[6] and self.board[0] != "":
-            return True
-        elif self.board[1] == self.board[4] == self.board[7] and self.board[1] != "":
-            return True
-        elif self.board[2] == self.board[5] == self.board[8] and self.board[2] != "":
-            return True
-
-        # Diagonal Win
-        elif self.board[0] == self.board[4] == self.board[8] and self.board[0] != "":
-            return True
-        elif self.board[2] == self.board[4] == self.board[6] and self.board[2] != "":
-            return True
+        for combo in win_combinations:
+            if combo.issubset(self.moves_done[self.current_player]):
+                return True
         
-        else:
-            return False
+        return False
         
     def check_for_drawn(self):
         for slot in self.board:
