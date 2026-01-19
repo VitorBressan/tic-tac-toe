34ea5d3177c9833713ac23df04010a35b8ce4f36
VitorBressan <contato.vbressan@gmail.com>
2026-01-19 14:04:41 -0300

    add: won verification and input value error handling, game fully working

--- game_engine.py
+++ game_engine.py
@@ -75,6 +75,33 @@ class GameEngine():
             return True
         else:
             return False
-        
+    
+    def check_for_win(self):
+        #[0][1][2]
+        #[3][4][5]
+        #[6][7][8]
 
+        # Horizontal Win
+        if self.board[0] == self.board[1] == self.board[3] and self.board[0] != "":
+            return True
+        elif self.board[3] == self.board[4] == self.board[5] and self.board[3] != "":
+            return True
+        elif self.board[6] == self.board[7] == self.board[8] and self.board[6] != "":
+            return True
         
+        # Vertical Win
+        if self.board[0] == self.board[3] == self.board[6] and self.board[0] != "":
+            return True
+        elif self.board[1] == self.board[4] == self.board[7] and self.board[1] != "":
+            return True
+        elif self.board[2] == self.board[5] == self.board[8] and self.board[2] != "":
+            return True
+
+        # Diagonal Win
+        elif self.board[0] == self.board[4] == self.board[8] and self.board[0] != "":
+            return True
+        elif self.board[2] == self.board[4] == self.board[6] and self.board[2] != "":
+            return True
+        
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from game_engine import GameEngine
 
-game = GameEngine()
+def ask_for_move():
+    valid = False
+    while not valid:
+        try:
+            move = int(input(f"{game.current_player} where do you want to play? ").strip())
+        except:
+            print("Please type a number from 1 to 9")
+        else:
+            valid = True
+    return move
 
+game = GameEngine()
 if game.start_game():
     game.get_names()
     
     while True:
         game.render_board()
         print(f"{game.current_player} turn!")
+        
         move_done = False
-
         while not move_done:
-            move = int(input(f"{game.current_player} where do you want to play? "))
+
+            move = ask_for_move()
+        
             if game.make_move(move):
                 move_done = True
             else:
                 print("Invalid Move! Please choose a valid slot!")
+
+        if game.check_for_win():
+            print(f"CONGRATULATIONS {game.current_player}! YOU'VE WON!")
+            break
 
         game.switch_player()
