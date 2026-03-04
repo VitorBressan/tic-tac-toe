6e04edec75f615fe60b1c50bc6bbd5a61fc2cfa1
VitorBressan <contato.vbressan@gmail.com>
2026-03-04 11:12:40 -0300

    add: add coverage, 100% testing for game_engine and add integration tests

--- tests/test_game_engine.py
+++ tests/test_game_engine.py
@@ -14,7 +14,7 @@ def test_game_starts_with_empty_board(game):
 def test_invalid_move_being_made(game):
     game.board = ["X"] * 9
     resp = game.make_move(0)
-    assert resp == False
+    assert resp == False 
 
 def test_valid_move_being_made(game):
     game.board = [""] * 9
@@ -22,15 +22,16 @@ def test_valid_move_being_made(game):
     assert resp == True
 
 def test_detect_winner(game):
-    game.moves_done[nameA] = {3, 4, 5}
-    game.current_player = nameA
+    game.moves_done[nameB] = {3, 4, 5}
     resp = game.detect_winner()
-    assert resp == nameA
+    assert resp == nameB
 
 def test_switch_player(game):
     assert game.current_player == nameA
     game.switch_player()
     assert game.current_player == nameB
+    game.switch_player()
+    assert game.current_player == nameA
 
 @pytest.mark.parametrize("combo", [
     {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
@@ -82,3 +83,55 @@ def test_win_has_priority_over_draw(game):
     game.moves_done[nameA] = {0, 4, 8}
 
     assert game.check_game_status() == ("win", nameA)
+
+def test_mark_slot(game):
+    resp = game.mark_slot()
+    assert resp == "X"
+    game.switch_player()
+    resp = game.mark_slot()
+    assert resp == "O"
+
+def test_is_valid_mode(game):
+    resp = game.is_valid_move(-1)
+    assert resp is False
+
+    resp = game.is_valid_move(5)
+    assert resp is True
+
+def test_full_game_win(game):
+    # A
+    game.make_move(0)
+    assert game.check_game_status() == ("ongoing", None)
+    game.switch_player()
+
+    # B
+    game.make_move(3)
+    assert game.check_game_status() == ("ongoing", None)
+    game.switch_player()
+
+    # A
+    game.make_move(1)
+    assert game.check_game_status() == ("ongoing", None)
+    game.switch_player()
+
+    # B
+    game.make_move(4)
+    assert game.check_game_status() == ("ongoing", None)
+    game.switch_player()
+
+    # A - winning move
+    game.make_move(2)
+    status = game.check_game_status()
+    assert status == ("win", nameA)
+
+
+def test_full_game_draw(game):
+    moves = [0,1,2,4,3,5,7,6,8]
+    for move in moves:
+        game.make_move(move)
+        status = game.check_game_status()
+        if status[0] != "ongoing":
+            break
+        game.switch_player()
+
+    assert status == ("draw", None)
